d4296c83fe2c4e6b31fc341ee9c8679fb2d17210
DIEGO GARCÍA PÉREZ <100405855@alumnos.uc3m.es>
2021-04-24 13:33:55 +0200

    Uploaded raspberry code

--- RaspberrypiCode/weatherstation.py
+++ RaspberrypiCode/weatherstation.py
@@ -1,19 +1,53 @@
+import paho.mqtt.client as mqtt
+import adafruit_dht
+import RPi.GPIO as GPIO
+import threading
+import uuid
+
+from publisher import *
+from utils.load_preferences import getPreferences
+from board import *
+
+
+SENSOR_PIN = D17
+DHT_SENSOR = adafruit_dht.DHT11(SENSOR_PIN, use_pulseio=False)
+
 client = mqtt.Client()
 
+def temperatureSensor():
+    while True:
+        try:
+            measured_temp = DHT_SENSOR.temperature
+            print("Temperature: " + str(measured_temp) + "ºC ")
+            send_temperature(measured_temp)
 
-def submit_data_to_store(data):
-    params = getPreferences("")
+            measured_humidity = DHT_SENSOR.humidity
+            print("Humidity: " + str(measured_humidity) + "%")
+            send_humidity(measured_humidity)
 
+            time.sleep(10)
+        except RuntimeError:
+            pass
 
-def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        print("Connected !!!!")
-    else:
-        print("Connection fail:", {rc})
+def humiditySensor():
+    while True:
+        try:
+            measured_humidity = DHT_SENSOR.humidity
+            print("Humidity: " + str(measured_humidity) + "%")
+            send_humidity(measured_humidity)
 
-def make_connection():
-    client.username_pw_set(username="diego", password="superdiego")
-    client.on_connect = on_connect
-    client.connect("34.89.255.226", 1883, 60)
+        except RuntimeError:
+            pass
 
-    print ("Connection has been made.")
+def weatherSensor():
+    make_connection()
+    id = ':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) for ele in range(0, 8 * 6, 8)][::-1])
+    print(id)
+    send_id(id + " - Raspberry 1")
+
+if __name__ == "__main__":
+    weatherSensor()
+    temperature = threading.Thread(target=temperatureSensor())
+    #    humidity = threading.Thread(target=humiditySensor())
+    temperature.start()
+# humidity.start()
